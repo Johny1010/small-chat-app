1e053521e90cf5f696b0c5326dcf1d7d712eed5a
Tomasz Hencel <you@example.com>
2015-12-08 21:30:38 +0100

    code relocated to different modules

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -186,6 +186,9 @@ spa.shell = (function (){
         $.uriAnchor.configModule({
            schema_map: configMap.anchor_schema_map
         });
+
+        spa.chat.configModule({});
+        spa.chat.initModule(jqueryMap.$chat);
         $(window)
             .bind('hashchange', onHashchange)
             .trigger('hashchange');
--- js/spa.util.js
+++ js/spa.util.js
@@ -1,3 +1,39 @@
 /**
  * Created by johny on 08.12.15.
  */
+spa.util = (function (){
+    var makeError, setConfigMap;
+
+    makeError = function (name_text, msg_text, data) {
+        var error = new Error ();
+        error.name = name_text;
+        error.message = msg_text;
+        if (data) {
+            error.data = data;
+        }
+        return error;
+    };
+
+    setConfigMap = function (arg_map) {
+        var input_map = arg_map.input_map,
+            settable_map = arg_map.settable_map,
+            config_map = arg_map.config_map,
+            key_name, error;
+
+      for (key_name in input_map){
+          if (input_map.hasOwnProperty(key_name)){
+              if (settable_map.hasOwnProperty(key_name)){
+                  config_map[key_name] = input_map[key_name];
+              }
+              else {
+                  error = makeError('Bledne dane wejsciowe', 'Ustawianie klucza konfiguracyjnego |' + key_name + '| nie jest obslugiwane');
+                  throw error;
+              }
+          }
+      }
+    };
+    return {
+        makeError: makeError,
+        setConfigMap: setConfigMap
+    };
+}());
